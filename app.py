3595b809604ece704409e0faf97528d210fc8919
Mostafa Razavi <mostafa@sepent.com>
2020-09-06 19:57:05 +0200

    Add Google login.
    Used this page as guide:
    https://realpython.com/flask-google-login/#creating-a-google-client

--- app.py
+++ app.py
@@ -1,7 +1,68 @@
 from woordteller import get_words
-from flask import Flask, request, render_template, redirect, session
+from flask import (
+    Flask, request, render_template, redirect, session, url_for
+)
+from flask_login import (
+    LoginManager, login_required, login_user, logout_user, current_user
+)
 from redis import Redis
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+import json
 import os
+
+
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+GOOGLE_DISCOVERY_URL = (
+    'https://accounts.google.com/.well-known/openid-configuration'
+)
+if not GOOGLE_CLIENT_ID:
+    raise RuntimeError('GOOGLE_CLIENT_ID environment variable not set.')
+if not GOOGLE_CLIENT_SECRET:
+    raise RuntimeError('GOOGLE_CLIENT_SECRET environment variable not set.')
+
+
+
+class User:
+    def __init__(self, id, name, email, picture):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.picture = picture
+
+        self.is_authenticated = True
+        self.is_active = True
+        self.is_anonymous = False
+
+
+    def get_id(self):
+        return self.id
+
+
+    @staticmethod
+    def get(id):
+        user_data = redis.get(f'user:{id}')
+        if user_data == None:
+            return None
+        else:
+            user_data = json.loads(user_data)
+            user = User(user_data['id'],
+                        user_data['name'],
+                        user_data['email'],
+                        user_data['picture'])
+            return user
+
+
+    @staticmethod
+    def create(id, name, email, picture):
+        user_data = {
+            'id': id,
+            'name': name,
+            'email': email,
+            'picture': picture,
+        }
+        redis.set(f'user:{id}', json.dumps(user_data))
 
 
 app = Flask(__name__)
@@ -14,12 +75,25 @@ else:
 redis_host = os.environ.get('REDIS_HOST', 'redis')
 redis = Redis(host=redis_host, decode_responses=True)
 
-user = 'foouser'
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
 @app.route('/')
-def get_root():
-    words_key = f'{user}:words'
+@login_required
+def index():
+    words_key = f'words:{current_user.get_id()}'
     if 'params' in session:
         params = session['params']
         del session['params']
@@ -30,12 +104,94 @@ def get_root():
     return render_template('home.html', **params)
 
 
+@app.route('/login')
+def login():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg['authorization_endpoint']
+
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + '/callback',
+        scope=['openid', 'email', 'profile'],
+    )
+    return redirect(request_uri)
+
+
+@app.route('/login/callback')
+def login_callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get('code')
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL from
+    # Google that gives you the user's profile information, including
+    # their Google profile image and email
+    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # You want to make sure their email is verified.  The user
+    # authenticated with Google, authorized your app, and now you've
+    # verified their email through Google!
+    if userinfo_response.json().get('email_verified'):
+        unique_id = userinfo_response.json()['sub']
+        users_email = userinfo_response.json()['email']
+        picture = userinfo_response.json()['picture']
+        users_name = userinfo_response.json()['given_name']
+    else:
+        return 'User email not available or not verified by Google.', 400
+
+    # Create a user in your db with the information provided by Google
+    user = User(
+        id=unique_id, name=users_name, email=users_email, picture=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for('index'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
 @app.route('/add', methods=['POST'])
-def post_add():
+@login_required
+def add_words():
     input_text = request.form['input-text']
     kept, thrown = get_words(input_text)
 
-    words_key = f'{user}:words'
+    words_key = f'words:{current_user.get_id()}'
     current = redis.smembers(words_key)
     new = kept - current
     existing = kept & current
@@ -55,4 +211,4 @@ def post_add():
 
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=8000, ssl_context='adhoc')

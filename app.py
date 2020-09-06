fde4d41eb8bd88fb76edb248e5729f5cf9395fdf
Mostafa Razavi <mostafa@sepent.com>
2020-09-06 18:06:39 +0200

    Use Post/Redirect/Get pattern for posting words.
    When posting new words, we now post to the /add end-point, which adds
    the words to the database, stores state in the session (i.e. cookies)
    and redirects to the original page. This way the original page can be
    safely bookmarked or refreshed even after post.
    
    We now need a secret key configured for signing cookies. This is read
    from the SECRET_KEY environment variable. When running using
    docker-compose, the value should be set in a .env file. An example
    .env.example file is included.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from woordteller import get_words
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, session
 from redis import Redis
 import os
 
 
 app = Flask(__name__)
+
+if os.environ.get('SECRET_KEY'):
+    app.config.update(SECRET_KEY=os.environ['SECRET_KEY'])
+else:
+    raise RuntimeError('Environment variable SECRET_KEY not set.')
+
 redis_host = os.environ.get('REDIS_HOST', 'redis')
 redis = Redis(host=redis_host, decode_responses=True)
 
@@ -14,14 +20,18 @@ user = 'foouser'
 @app.route('/')
 def get_root():
     words_key = f'{user}:words'
-    params = {
-        'nwords': redis.scard(words_key),
-    }
+    if 'params' in session:
+        params = session['params']
+        del session['params']
+    else:
+        params = {
+            'nwords': redis.scard(words_key),
+        }
     return render_template('home.html', **params)
 
 
-@app.route('/', methods=['POST'])
-def post_root():
+@app.route('/add', methods=['POST'])
+def post_add():
     input_text = request.form['input-text']
     kept, thrown = get_words(input_text)
 
@@ -34,12 +44,14 @@ def post_root():
         redis.sadd(words_key, new_word)
 
     params = {
-        'new_words': new,
-        'existing_words': existing,
-        'thrown_words': thrown,
+        'new_words': list(new),
+        'existing_words': list(existing),
+        'thrown_words': list(thrown),
         'nwords': redis.scard(words_key),
     }
-    return render_template('home.html', **params)
+
+    session['params'] = params
+    return redirect('/', code=303)
 
 
 if __name__ == '__main__':

797f0ab12e5896903ca7351076923ad39fa72704
Mostafa Razavi <mostafa@sepent.com>
2020-09-13 20:49:13 +0200

    Re-split phrases again.
    Frog sometimes returns phrases instead of words, e.g. "zeg maar" will
    be returned as "zeggen_maar" with the NER value being "O_O", so two
    types are actually returned separated by an underscore. We split these
    phrases again and add the "O" types to the "kept" list.

--- woordenteller.py
+++ woordenteller.py
@@ -14,9 +14,24 @@ def get_words(s, debug=False):
         elif word['ner'] == 'O':
             kept.add(word['lemma'])
         else:
-            to_add = word['lemma']
-            if debug:
-                to_add += f' ({word["ner"]})'
-            thrown.add(to_add)
+            if '_' in word['ner']:
+                # sometimes phrases are returned instead of words,
+                # e.g. "zeg maar" will be returned as "zeggen_maar"
+                # with the NER value being "O_O", so two types are
+                # actually returned separated by an underscore. We
+                # split these phrases again and add the "O" types to
+                # the "kept" list.
+                words = word['lemma'].split('_')
+                types = word['ner'].split('_')
+                for word, type in zip(words, types):
+                    if type == 'O':
+                        kept.add(word)
+                    else:
+                        thrown.add(word + (f' ({type})' if debug else ''))
+            else:
+                to_add = word['lemma']
+                if debug:
+                    to_add += f' ({word["ner"]})'
+                thrown.add(to_add)
 
     return kept, thrown
